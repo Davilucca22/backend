7b9eca0e03c0053dd50cec0d20a241e9362ed2db
lucca <davilucarios47@gmail.com>
2026-01-02 11:06:04 -0300

    curtidas atualizadas

--- controllers/cutircontroller.js
+++ controllers/cutircontroller.js
@@ -6,21 +6,4 @@ export const attcurtida = async (req,res) =>{
 
     const post = await User.findOneAndUpdate({'posts._id':postId},{$inc:{'posts.$.curtidas':1}},{new:true})
 
-    const page = Number(req.query.page) || 1 //pega a query do endpoint ou 1
-    const limit = 10 // limite de paginas
-    const skip = (page - 1) * limit //pula paginas anteriores
-    
-    const totalUsers = await User.countDocuments() // total de documentos no BD
-    
-    const users = await User.find()
-    .sort({criadoem: -1}) //mais recente antes
-    .skip(skip)
-    .limit(limit)
-    
-    res.json({
-        page,
-        totalPage: Math.ceil(totalUsers / limit),
-        data: users
-    })
-
 }
--- controllers/feedcontroller.js
+++ controllers/feedcontroller.js
@@ -2,23 +2,29 @@ import User from "../models/UserModel.js"
 
 export const Feed = async (req,res) => {
     try{
-        
-        const page = Number(req.query.page) || 1 //pega a query do endpoint ou 1
-        const limit = 10 // limite de paginas
-        const skip = (page - 1) * limit //pula paginas anteriores
-        
-        const totalUsers = await User.countDocuments() // total de documentos no BD
-        
-        const users = await User.find()
-        .sort({criadoem: -1}) //mais recente antes
-        .skip(skip)
-        .limit(limit)
-        
-        res.json({
-            page,
-            totalPage: Math.ceil(totalUsers / limit),
-            data: users
-        })
+
+        const feed = await User.aggregate([
+            {$sample:{size:10}}, //pega usuarios aleatorios
+            {$unwind:"$posts"}, //transforma array de posts em docs individuais
+            {$sample:{size:30}}, //posts aleatorios desses usuarios
+            {$project:{ //estrutura que ira para o front
+                _id:0,
+                userId:"$_id",
+                name: 1,
+                fotoPerfil: 1,
+                post:{
+                    _id:"$posts._id",
+                    imgURL:"$posts.imgURL",
+                    textoPost:"$posts.textoPost",
+                    criadoem:"$posts.criadoem",
+                    curtidas:"$posts.curtidas",
+                    comentarios:"$posts.comentarios"
+                }
+            }}  
+
+        ])
+
+        res.json(feed)
 
     }catch(e){
         console.log(e)

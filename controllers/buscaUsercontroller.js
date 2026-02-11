76810e152af75c7749c48cebca16f44341adf309
lucca <davilucarios47@gmail.com>
2026-02-10 22:08:41 -0300

    quase terminando

--- controllers/buscaUsercontroller.js
+++ controllers/buscaUsercontroller.js
@@ -3,7 +3,8 @@ import User from "../models/UserModel.js"
 export const Busca = async (req,res) =>{
     const { nome } = req.query
 
-    const users = await User.find({name:{ $regex: nome, $options:'i'}}).select('-senha -email -posts -seguindo -seguidores -dataNasc')
+    const users = await User.find({name:{ $regex: nome, $options:'i'}}) //regex verifica se contem a string passada na chave; options ignora M e m
+    .select('-senha -email -posts -seguindo -seguidores -dataNasc') // tira esses dados do retorno pro front
 
     console.log(users)
 

fe49640374c2d750d50482d651c229bf28e26938
lucca <davilucarios47@gmail.com>
2026-04-20 11:56:49 -0300

    Update feedcontroller.js

--- controllers/feedcontroller.js
+++ controllers/feedcontroller.js
@@ -2,9 +2,11 @@ import User from "../models/UserModel.js"
 
 export const Feed = async (req,res) => {
     try{
+        const userId = req.user.id; // ID do usuário logado
 
         const feed = await User.aggregate([
-            {$sample:{size:10}}, //pega usuarios aleatorios
+            {$match: {_id: {$ne: userId}}}, // Exclui o próprio usuário
+            {$sample:{size:10}}, //pega usuarios aleatorios (excluindo o próprio)
             {$unwind:"$posts"}, //transforma array de posts em docs individuais
             {$sample:{size:30}}, //posts aleatorios desses usuarios
             {$project:{ //estrutura que irá para o front
